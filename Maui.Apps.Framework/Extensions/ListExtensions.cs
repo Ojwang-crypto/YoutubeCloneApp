627a319e133f229fd5800ae08df0666d5857a1c7
Ojwang-crypto <omondicollins163@gmail.com>
2023-04-10 18:12:43 +0300

    Added concerters

--- Maui.Apps.Framework/Extensions/ListExtensions.cs
+++ Maui.Apps.Framework/Extensions/ListExtensions.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Maui.Apps.Framework.Extensions;
 
-namespace Maui.Apps.Framework.Extensions
+public static class ListExtensions
 {
-    internal class ListExtensions
+    public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> newItems, bool clearFirst = false)
     {
+        if (clearFirst)
+            collection.Clear();
+
+        newItems.ForEach(newItem => collection.Add(newItem));
+    }
+
+    public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source)
+    {
+        Random rnd = new Random();
+        return source.OrderBy<T, int>((item) => rnd.Next());
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+    {
+        foreach (var item in enumeration)
+        {
+            action(item);
+        }
     }
 }
--- Maui.Apps.Framework/GlobalUsings.cs
+++ Maui.Apps.Framework/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using System.Text.Json;
 global using System.Text;
 global using Maui.Apps.Framework.Exceptions;
 global using Maui.Apps.Framework.Extensions;
+global using System.Text.RegularExpressions;
+global using System.Collections.ObjectModel;
+global using System.Globalization;
